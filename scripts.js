c21dc5f5970aaaada8b045b8e63a03b3647fe927
CarlaPinheiro <carlampinheiro@hotmail.com>
2017-03-16 22:10:15 +0000

    first commit

---
+++ scripts.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    $('.btn').on('click', function(){  
+          $('.list li').fadeToggle()
+    
+    }) 
+
+    
+//         ('.list li').append( <li>'Item 4'</li>) {       
+        /* $(this).addClass('active');*/
+                            
+
+    
+   /* $('.btn').on('click', function() {       
+         $(this).text('hello');
+    }) 
+*/
+    
+    
+     /*$('.list li').on('click', function() {       
+         $(this).addClass('active');
+    })*/
+   
+    
+     
+   /* $('.btn').on('click', function() {*/
+    /*    $('.list li').addClass('active')
+    })
+    */
+     
+     
+    /* $('.list li').addClass('active')*/
+    
+    })
